ab8163b85f2ba052b84ea4a295985b88c9e07d93
ManavKalariya <manavkalariya4444@gmail.com>
2024-10-21 21:19:29 +0530

    Creating Project Repo
    CRUD for Medicines, Prescriptions and Orders completed.

---
+++ Pharmacy/Startup.cs
@@ -0,0 +1,40 @@
+﻿//using AutoMapper.Configuration;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Pharmacy.Helpers;
+using Pharmacy.Models;
+
+namespace Pharmacy
+{
+    public class Startup
+    {
+        private IConfiguration configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureAppByDefault(configuration);
+            //services.AddDbContextPool<AppDbContext>(
+             //  options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            //services.AddControllersWithViews();
+           // services.AddScoped<IStudentRepository, SQLStudentRepository>();
+        }
+            
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}"));
+        }
+    }
+}
